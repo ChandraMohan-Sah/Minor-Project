31ff29e78560d99831fed81412dfb05325cbad98
ChandraMohan-Sah <csah9628@gmail.com>
2024-11-04 10:11:22 +0545

    Little changes

--- RoutePlot_APP/api/views.py
+++ RoutePlot_APP/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status 
 from rest_framework import generics 
 
 from RoutePlot_APP.models import StationInfo, RouteInfo, RouteStationInfo
