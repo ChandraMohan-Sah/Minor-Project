67a60a179991f1d2f1a9587619d9bedd3ed067bb
ChandraMohan-Sah <csah9628@gmail.com>
2024-11-04 11:07:25 +0545

    Added Function for get_bus_location

--- API_CONSUMER/urls.py
+++ API_CONSUMER/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from .views import  Post_GPS_Location, Get_Routes, Get_Stations_on_Route
+from .views import  Post_GPS_Location, Get_Routes, Get_Stations_on_Route, bus_location_view
 
 urlpatterns = [
     path('', Get_Routes, name="map"),
     path('post/<int:deviceid>/<str:latitude>/<str:longitude>/', Post_GPS_Location, name="gpsdata"),
-    path('post_to_get_stationsname_for_particular_route/<int:routenumber>/', Get_Stations_on_Route, name="stations-data" )
-    # path('home/', home, name="home")
-    
+    path('post_to_get_stationsname_for_particular_route/<int:routenumber>/', Get_Stations_on_Route, name="stations-data" ),
+    path('bus_location/<int:routenumber>/', bus_location_view, name='bus_location'),
+    #path('home/', home, name="home")
 ]
  
 
- 
 
- 
